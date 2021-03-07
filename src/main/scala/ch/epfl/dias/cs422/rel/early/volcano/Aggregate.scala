b3b3eeda139be7ecaf9e538e682e86e8304d6072
Wermeille Bastien <bastien.wermeille@gmail.com>
2021-03-08 00:23:34 +0100

    speedup ?

--- src/main/scala/ch/epfl/dias/cs422/rel/early/volcano/Aggregate.scala
+++ src/main/scala/ch/epfl/dias/cs422/rel/early/volcano/Aggregate.scala
@@ -12,15 +12,15 @@ import org.apache.calcite.util.ImmutableBitSet
   * @see [[ch.epfl.dias.cs422.helpers.rex.AggregateCall]]
   */
 class Aggregate protected (
-    input: ch.epfl.dias.cs422.helpers.rel.early.volcano.Operator,
-    groupSet: ImmutableBitSet,
-    aggCalls: IndexedSeq[AggregateCall]
-) extends skeleton.Aggregate[
-      ch.epfl.dias.cs422.helpers.rel.early.volcano.Operator
-    ](input, groupSet, aggCalls)
-    with ch.epfl.dias.cs422.helpers.rel.early.volcano.Operator {
+                            input: ch.epfl.dias.cs422.helpers.rel.early.volcano.Operator,
+                            groupSet: ImmutableBitSet,
+                            aggCalls: IndexedSeq[AggregateCall]
+                          ) extends skeleton.Aggregate[
+  ch.epfl.dias.cs422.helpers.rel.early.volcano.Operator
+](input, groupSet, aggCalls)
+  with ch.epfl.dias.cs422.helpers.rel.early.volcano.Operator {
 
-  protected var aggregatedIterator = Iterator.empty[Tuple]
+  protected var aggregated = List.empty[(Tuple, Vector[Tuple])]
 
   /**
     * @inheritdoc
@@ -30,36 +30,30 @@ class Aggregate protected (
     var next = input.next()
     if (next == NilTuple && groupSet.isEmpty) {
       // return aggEmptyValue for each aggregate.
-      aggregatedIterator = Vector(
-        aggCalls
-          .map(aggEmptyValue)
-          .foldLeft(IndexedSeq.empty[Elem])((a, b) => a :+ b)
-          .asInstanceOf[Tuple]
-      ).iterator
+      aggregated = List(
+        (IndexedSeq.empty[Elem] -> Vector(
+          aggCalls
+            .map(aggEmptyValue)
+            .foldLeft(IndexedSeq.empty[Elem])((a, b) => a :+ b)
+            .asInstanceOf[Tuple]
+        ))
+      )
 
     } else {
       // Group based on the key produced by the indices in groupSet
       val keyIndices = groupSet.toArray
-      var aggregates = Map.empty[Tuple, Tuple]
+      var aggregates = Map.empty[Tuple, Vector[Tuple]]
       while (next != NilTuple) {
         val tuple: Tuple = next.get
         val key: Tuple = keyIndices.map(i => tuple(i))
         aggregates = aggregates.get(key) match {
-          case Some(arr: Tuple) => {
-            aggregates + (key -> aggCalls.zip(arr).map {
-              case (agg, t) =>
-                aggReduce(t, agg.getArgument(tuple), agg)
-            })
-          }
-          case _ =>
-            aggregates + (key -> aggCalls.map(agg => agg.getArgument(tuple)))
+          case Some(arr: Vector[Tuple]) => aggregates + (key -> (arr :+ tuple))
+          case _                        => aggregates + (key -> Vector(tuple))
         }
         next = input.next()
       }
 
-      aggregatedIterator = aggregates.toArray.map {
-        case (key, tuple) => key.++(tuple)
-      }.iterator
+      aggregated = aggregates.toList
     }
   }
 
@@ -67,10 +61,17 @@ class Aggregate protected (
     * @inheritdoc
     */
   override def next(): Option[Tuple] = {
-    if (aggregatedIterator.hasNext) {
-      Some(aggregatedIterator.next)
-    } else {
-      NilTuple
+    aggregated match {
+      case (key, tuples) :: tail =>
+        aggregated = tail
+        Some(
+          key.++(
+            aggCalls.map(agg =>
+              tuples.map(t => agg.getArgument(t)).reduce(aggReduce(_, _, agg))
+            )
+          )
+        )
+      case _ => NilTuple
     }
   }
 
@@ -78,4 +79,3 @@ class Aggregate protected (
     * @inheritdoc
     */
   override def close(): Unit = input.close()
-}
